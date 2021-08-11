c5b7af05fb012d6e8cd3cc46889bc20343bd2ac1
jhell85 <jhell85@gmail.com>
2021-08-11 14:15:28 -0700

    placed app in container

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import { render } from "react-dom";
+import { Container } from "@material-ui/core";
 
 import HomePage from "./HomePage";
 
@@ -9,9 +10,9 @@ export default class App extends Component {
   }
   render() {
     return (
-      <div>
+      <Container>
         <HomePage />
-      </div>
+      </Container>
     );
   }
 }
